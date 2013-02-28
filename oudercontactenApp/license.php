a0c271fb7b992c919938d8531690977beeb9c022
gb@dbz.be <>
2013-02-28 16:00:44 +0100

    license

---
+++ oudercontactenApp/license.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * LICENSE
+ *
+ * This source file is subject to the license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://dbz.be/license/oudercontact
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@dbz.be so we can send you a copy immediately.
+ *
+ * @category   dbz
+ * @package    dbz_oudercontact
+ * @copyright  Copyright (c) 2011 Don Boscocollege Zwijnaarde (http://dbz.be)
+ * @license    http://dbz.be/license/oudercontact
+ * @version    $Id: Console.php 20096 2010-01-06 02:05:09Z bkarwin $
+ */
+?>
