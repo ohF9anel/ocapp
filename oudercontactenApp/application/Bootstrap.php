a953a57712ad357ea37ac74ec1bc648607764732
gb@dbz.be <>
2013-03-01 11:25:03 +0100

    added optional function to enable more error logging uncomment to activate

--- oudercontactenApp/application/Bootstrap.php
+++ oudercontactenApp/application/Bootstrap.php
@@ -2,7 +2,14 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
+     // to enable error logging you could use uncomment this:
+  
+     /*
+        protected function _initErrorDisplay(){
+          $frontController = Zend_Controller_Front::getInstance();
+          $frontController->throwExceptions(true);
+        }
+     */
 
 }
 
